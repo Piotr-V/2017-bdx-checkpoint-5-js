c05f8f49b99e113b0e5f7c380b3d4b56e4078808
Piotr-V <pierrevicq@hotmail.fr>
2017-07-12 15:32:45 +0200

    not even close

--- public/js/components/algo/algo1.js
+++ public/js/components/algo/algo1.js
@@ -1,16 +1,16 @@
 let algo1 = {
     templateUrl: 'js/components/algo/algo1.html',
-    controller: ['UsersService', '$state', function(UsersService, $state) {
+    controller: ['UsersService', '$state', function (UsersService, $state) {
         'use strict'
         angular.extend(this, {
             $onInit() {
-
                 UsersService.getCurrent().then((user) => {
                     this.user = user
                 }).catch((err) => {
 
                 })
             },
+
             disconnect() {
                 UsersService.disconnect().then(() => {
                     Materialize.toast('Disconnected', 4000, 'toast-warning')
@@ -19,7 +19,17 @@ let algo1 = {
                 })
             }
 
-        })
+        });
+        
+        // this.friend = (this.friends) => {
+        // bestFriends = [];
+        //     for (let i = 0; i < friends.length; i++) {
+        //         if (this.friends[i].length == 4) {
+        //         bestFriends.push(this.friends[i]);
+        //         }       
+        //     }
+        // return bestFriends;
+        // }   
     }]
 }
 
